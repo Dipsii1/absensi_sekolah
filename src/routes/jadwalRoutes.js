eb4a506bc688117831444fc32c74e72732483450
Dipsii1 <nadhifaryastya1@gmail.com>
2026-04-28 10:18:40 +0700

    add checkrole in routes jadwal

--- src/routes/jadwalRoutes.js
+++ src/routes/jadwalRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const jadwalControllers = require("../controllers/jadwalControllers");
+const { verifyToken, checkRole } = require("../middleware/auth");
 
 router.get("/", jadwalControllers.getAllJadwal);
-router.post("/", jadwalControllers.createJadwal);
-router.put("/:id", jadwalControllers.updateJadwal);
-router.delete("/:id", jadwalControllers.deleteJadwal);
+router.post("/", verifyToken, checkRole(["admin"]), jadwalControllers.createJadwal);
+router.put("/:id", verifyToken, checkRole(["admin"]), jadwalControllers.updateJadwal);
+router.delete("/:id", verifyToken, checkRole(["admin"]), jadwalControllers.deleteJadwal);
 
 module.exports = router;
