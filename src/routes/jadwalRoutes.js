be1861836b528d27ce761f0eb57e21f4357f3561
Dipsii1 <nadhifaryastya1@gmail.com>
2026-05-03 14:22:26 +0700

    fix: error in route jadwal routes import jadwalControllers

--- src/routes/jadwalRoutes.js
+++ src/routes/jadwalRoutes.js
@@ -1,17 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const upload = require("../middleware/upload");
-const {
-  getAllJadwal,
-  createJadwal,
-  updateJadwal,
-  deleteJadwal,
-  importJadwal,
-} = require("../controllers/jadwalControllers");
+const jadwalControllers = require("../controllers/jadwalControllers");
 
 router.get("/", jadwalControllers.getAllJadwal);
 router.post("/", jadwalControllers.createJadwal);
-router.post("/import", jadwalControllers.uploadXlsx.single("file"), jadwalControllers.importJadwal);
+router.post("/import", upload.single("file"), jadwalControllers.importJadwal);
 router.put("/:id", jadwalControllers.updateJadwal);
 router.delete("/:id", jadwalControllers.deleteJadwal);
 
