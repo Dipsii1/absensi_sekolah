55e76ed5d0e794d109cc307a7fac8e1527db4a8f
Dipsii1 <nadifaryastia@gmail.com>
2026-08-22 15:59:59 +0700

    fix: deleted listen server from app.js

--- app.js
+++ app.js
@@ -87,9 +87,5 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
 
 module.exports = app;
