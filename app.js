f362d40bcfa425bfb5b99fff3f05544f7ea7cb0c
Riza Pahlevi <rizagapakel30@gmail.com>
2026-08-23 05:23:19 +0700

    fix: Allowed CORS origin, allowing IP network staging

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors({
-  origin: process.env.URL_FRONTEND?.trim() || 'http://localhost:4321',
+  origin: 'http://10.1.128.31' || process.env.URL_FRONTEND?.trim(),
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept']
